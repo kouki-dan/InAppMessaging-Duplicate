8e1adfef9e138cd295f97b5d05c96c826f514396
Kouki Saito <dan.addr.skd@gmail.com>
2021-12-09 22:33:29 +0900

    Initial Commit

---
+++ Shared/InAppMessaging_DuplicateApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppMessaging_DuplicateApp.swift
+//  Shared
+//
+//  Created by Kouki Saito on 2021/12/09.
+//
+
+import SwiftUI
+
+@main
+struct InAppMessaging_DuplicateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
